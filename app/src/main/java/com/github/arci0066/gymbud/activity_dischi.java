ce0258257793c67a47116c188c7a8379f92cba1e
Lorenzo Arcidiacono <arci0066@gmail.com>
2022-02-24 11:37:17 +0100

    Initial commit

---
+++ app/src/main/java/com/github/arci0066/gymbud/activity_dischi.java
@@ -0,0 +1,14 @@
+package com.github.arci0066.gymbud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_dischi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dischi);
+    }
+}
---
+++ app/src/main/java/com/github/arci0066/gymbud/activity_percentuali.java
@@ -0,0 +1,14 @@
+package com.github.arci0066.gymbud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_percentuali extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percentuali);
+    }
+}
